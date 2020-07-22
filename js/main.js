6eecb44cfe740194c8fb27d3c989bfcf8bb4f297
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-07-22 12:02:36 +0300

    gh-pages

--- js/main.js
+++ js/main.js
@@ -50,6 +50,7 @@ const myRand = () => {
 for (let i = 0; i < 50; i++) {
   const delay = Math.random() + "s";
   const el = document.createElement("img");
+  
   el.src = "../assets/img/star.PNG";
   el.className = "glitter-star";
   el.style.top = myRand() + "%";
